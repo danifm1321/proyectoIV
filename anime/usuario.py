21bfe6bcda51fc96804e0c0d0a12440c1cd27f2a
danifm1321 <danifm13@correo.ugr.es>
2021-11-05 13:16:40 +0100

    Diseño clase usuario closes #11

---
+++ anime/usuario.py
@@ -0,0 +1,43 @@
+# Fichero que contiene la clase Usuario
+
+
+
+class Usuario:
+    
+    '''
+    Clase que representa a un usuario
+    
+    Argumentos
+    ----------
+    usuario: str
+        Nombre de usuario
+    contrasena: str
+        Contraseña del usuario
+    visto: list[(int, float)]
+        Historial de animes vistos por el usuario
+    recomendaciones: list[int]
+        Historial de recomendaciones realizadas al usuario
+    '''
+
+    def __init__(self, usuario, contrasena, vistos, recomendaciones):
+
+        '''
+        Construye objeto Usuario proporcionando valores para todos sus atributos
+
+        Argumentos
+        ----------
+        usuario: str
+            Nombre de usuario
+        contrasena: str
+            Contraseña del usuario
+        visto: list[(int, float)]
+            Historial de animes vistos por el usuario
+        recomendaciones: list[int]
+            Historial de recomendaciones realizadas al usuario
+        '''
+
+        self.usuario = usuario
+        self.contrasena = contrasena
+        self.vistos = vistos
+        self.recomendaciones = recomendaciones
+        
